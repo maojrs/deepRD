d52bc031f5963affd1fe6234cd0d81e02c0869f1
maojrs <maojrs@gmail.com>
2021-09-13 16:30:28 +0200

    simplified noise sampler class

--- deepRD/noiseSampler/noiseSampler.py
+++ deepRD/noiseSampler/noiseSampler.py
@@ -3,11 +3,15 @@ import numpy as np
 class noiseSampler:
     '''
     Main class to sample noise from data-based models. The sample function takes
-    conditionedVariables as input. This is a list with all the relevant variable for
-    a given implementation
+    conditionedVariables as input, which is a list with all the relevant variable for
+    a given implementation. The sampling model can be arbitrary, it only needs to have
+    a function called sample that takes conditioned variables as input.
     '''
-    def __init__(self, sampleDimension = 3):
-        self.sampleDimension = sampleDimension
+    def __init__(self, samplingModel= None):
+        self.samplingModel = samplingModel
 
     def sample(self, conditionedVariables):
-        return np.random.normal(0., 1, self.sampleDimension)
+        try:
+            return self.samplingModel.sample(conditionedVariables)
+        except:
+            return np.random.normal(0., 1, 3)
