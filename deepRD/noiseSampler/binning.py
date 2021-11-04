c064f7e5677ac8aabbd2dfd11e6a704e8239092e
maojrs <maojrs@gmail.com>
2021-11-04 18:17:24 +0100

    Added support for more conditioned variables

--- deepRD/noiseSampler/binning.py
+++ deepRD/noiseSampler/binning.py
@@ -37,7 +37,6 @@ class binnedData:
         self.posIndex = 1  # Index of x position coordinate in trajectory files
         self.velIndex = 4  # Index of x velocity coordinate in trajectory files
         self.auxIndex = 8  # Position of x coordinate of r in trajectory files
-        self.neighborsDictionary = {}
         self.dataTree = None # dataTree structure to find nearest neighbors
         self.occupiedTuplesArray = None # array of tuples corresponding to occupied bins
         self.parameterDictionary = {}
