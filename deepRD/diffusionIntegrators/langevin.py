cb79b4bf57b839e759027c493c48403344744605
maojrs <maojrs@gmail.com>
2021-12-02 18:00:35 +0100

    Upgrade Langevin integrator

--- deepRD/diffusionIntegrators/langevin.py
+++ deepRD/diffusionIntegrators/langevin.py
@@ -4,41 +4,27 @@ from .diffusionIntegrator import diffusionIntegrator
 
 class langevin(diffusionIntegrator):
     '''
-    Integrator class to integrate the diffusive dynamics of a Brownian particle (full Langevin dynamics)
+    Integrator class to integrate the diffusive dynamics of a Brownian particle (full Langevin dynamics). This
+    is the parent class, which implement the BAOAB method by default. Other methods are available through
+    the child classes
     '''
 
-    def __init__(self, dt, stride, tfinal, kBT=1, boxsize = None, boundary = 'periodic',
-                 integratorType="BAOAB", equilibrationSteps = 0):
+    def __init__(self, dt, stride, tfinal, Gamma, kBT=1, boxsize = None, boundary = 'periodic', equilibrationSteps = 0):
         # inherit all methods from parent class
         super().__init__(dt, stride, tfinal, kBT, boxsize, boundary)
-        self.integratorType = integratorType
+        self.Gamma = Gamma # friction coefficient in units of mass/time
         self.equilibrationSteps = equilibrationSteps
 
     def integrateOne(self, particleList):
-        ''' Integrates one time step '''
-        # Integrate BAOAB
-        if self.integratorType == "BAOAB":
-            self.integrateB(particleList)
-            self.integrateA(particleList)
-            self.integrateO(particleList)
-            self.integrateA(particleList)
-            self.enforceBoundary(particleList)
-            particleList.updatePositions()
-            self.integrateB(particleList)
-            particleList.updateVelocities()
-        elif self.integratorType == "ABOBA":
-            self.integrateA(particleList)
-            particleList.updatePositions()
-            self.integrateB(particleList)
-            self.integrateO(particleList)
-            self.integrateB(particleList)
-            self.integrateA(particleList)
-            self.enforceBoundary(particleList)
-            particleList.updatePositionsVelocities()
-        elif self.integratorType == "symplecticEuler":
-            self.integrateOneSymplecticEuler(particleList)
-            self.enforceBoundary(particleList)
-            particleList.updatePositionsVelocities()
+        ''' Integrates one time step using the BAOAB algorithm '''
+        self.integrateB(particleList)
+        self.integrateA(particleList)
+        self.integrateO(particleList)
+        self.integrateA(particleList)
+        self.enforceBoundary(particleList)
+        particleList.updatePositions()
+        self.integrateB(particleList)
+        particleList.updateVelocities()
 
     def propagate(self, particleList, showProgress = False):
         #percentage_resolution = self.tfinal / 100.0
@@ -83,20 +69,49 @@ class langevin(diffusionIntegrator):
     def integrateO(self, particleList):
         '''Integrates velocity full time step given friction and noise term'''
         for particle in particleList:
-            eta = self.kBT / particle.D # friction coefficient
-            xi = np.sqrt(self.kBT * particle.mass * (1 - np.exp(-2 * eta * self.dt/particle.mass)))
-            frictionTerm = np.exp(-self.dt * eta/particle.mass) * particle.nextVelocity
+            xi = np.sqrt(self.kBT * particle.mass * (1 - np.exp(-2 * self.Gamma * self.dt/particle.mass)))
+            frictionTerm = np.exp(-self.dt * self.Gamma/particle.mass) * particle.nextVelocity
             particle.nextVelocity = frictionTerm + xi / particle.mass * np.random.normal(0., 1, particle.dimension)
 
-    def integrateOneSymplecticEuler(self, particleList):
+
+class langevinBAOAB(langevin):
+    '''
+    Same as langevin class, but method stated explicitly. Uses BAOAB method for integration.
+    '''
+    pass
+
+
+class langevinABOBA(langevin):
+    '''
+    Same as langevin class, but method stated explicitly. Uses ABOBA method for integration.
+    '''
+    def integrateOne(self, particleList):
+        ''' Integrates one time step using the ABOBA algorithm '''
+        self.integrateA(particleList)
+        particleList.updatePositions()
+        self.integrateB(particleList)
+        self.integrateO(particleList)
+        self.integrateB(particleList)
+        self.integrateA(particleList)
+        self.enforceBoundary(particleList)
+        particleList.updatePositionsVelocities()
+
+class langevinSemiImplicitEuler(langevin):
+    '''
+    Same as langevin class, but method stated explicitly. Uses the semi-implicit Euler method for integration.
+    '''
+    def integrateOne(self, particleList):
+        ''' Integrates one time step using the semi-eimplicit Euler algorithm '''
         forceField = self.calculateForceField(particleList)
         for i, particle in enumerate(particleList):
             force = forceField[i]
-            eta = self.kBT / particle.D  # friction coefficient
-            xi = np.sqrt(2 * self.kBT * eta * self.dt )
-            frictionTerm = -(self.dt * eta / particle.mass) * particle.nextVelocity
+            xi = np.sqrt(2 * self.kBT * self.Gamma * self.dt)
+            frictionTerm = -(self.dt * self.Gamma / particle.mass) * particle.nextVelocity
             particle.nextVelocity = particle.nextVelocity + self.dt * (force / particle.mass) + \
                                     frictionTerm + (xi / particle.mass) * np.random.normal(0., 1, particle.dimension)
         for particle in particleList:
             particle.nextPosition = particle.nextPosition + self.dt * particle.nextVelocity
+        self.enforceBoundary(particleList)
+        particleList.updatePositionsVelocities()
+
 
