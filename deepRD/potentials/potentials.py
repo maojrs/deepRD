633a9b6ed481912b78bffacd5e345d3d66ba4e45
maojrs <maojrs@gmail.com>
2022-04-19 20:55:40 +0200

    Adde combined potential

--- deepRD/potentials/potentials.py
+++ deepRD/potentials/potentials.py
@@ -1,3 +1,4 @@
+import numpy as np
 '''
 Parent class of external potentials. The potentials' functions usually take as input one or two
 particles, depending if it is an external potential or a pair potential.
@@ -51,3 +52,56 @@ class pairPotential:
                 if (pos2[i] - pos1[i]) < -0.5 * self.boxsize[i]:
                     p1Periodic[i] -= self.boxsize[i]
         return pos2 - p1periodic
+
+
+class combinedExternalPotential:
+    '''
+    Class to combine several external potentials in one
+    '''
+    def __init__(self):
+        self.potentials = []
+
+    def addPotential(self, potential):
+        if isinstance(potential, list):
+            self.potentials = self.potentials + potential
+        else:
+            self.potentials.append(potential)
+
+    def evaluate(self, particle):
+        potentialVal = 0.0
+        for i in range(len(self.potentials)):
+            potentialVal += self.potentials[i].evaluate(particle)
+        return potentialVal
+
+    def calculateForce(self, particle, currentOrNext = 'current'):
+        dimension = len(particle.position)
+        forceVal = np.zeros(dimension)
+        for i in range(len(self.potentials)):
+            forceVal += self.potentials[i].calculateForce(particle)
+        return forceVal
+
+class combinedPaiPotential:
+    '''
+    Class to combine several external potentials in one
+    '''
+    def __init__(self):
+        self.potentials = []
+
+    def addPotential(self, potential):
+        if isinstance(potential, list):
+            self.potentials = self.potentials + potential
+        else:
+            self.potentials.append(potential)
+
+    def evaluate(self, particle1, particle2):
+        potentialVal = 0.0
+        for i in range(len(self.potentials)):
+            potentialVal += self.potentials[i].evaluate(particle1, particle2)
+        return potentialVal
+
+    def calculateForce(self, particle1, particle2, currentOrNext = 'current'):
+        dimension = len(particle1.position)
+        forceVal = np.zeros(dimension)
+        for i in range(len(self.potentials)):
+            forceVal += self.potentials[i].calculateForce(particle1, particle2, currentOrNext)
+        return forceVal
