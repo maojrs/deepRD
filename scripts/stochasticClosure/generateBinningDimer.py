4d34e4827dcc9dace2d84455a95731ba8df28ce6
maojrs <maojrs@gmail.com>
2022-06-15 18:28:58 +0200

    Fixed bug in script

--- scripts/stochasticClosure/generateBinningDimer.py
+++ scripts/stochasticClosure/generateBinningDimer.py
@@ -31,7 +31,7 @@ except OSError as error:
 # Load parameters from parameters file
 parameterDictionary = analysisTools.readParameters(parentDirectory + "parameters")
 # Parameters for loading continuous trajectories from files (from original simulation)
-nfiles = 1 #parameterDictionary['numFiles']
+nfiles = parameterDictionary['numFiles']
 dt = parameterDictionary['dt']
 stride = parameterDictionary['stride']
 totalTimeSteps = parameterDictionary['timesteps']
