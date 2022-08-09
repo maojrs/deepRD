fd084c528be708e01acd43ead7621920c1812828
maojrs <maojrs@gmail.com>
2022-08-09 15:07:24 +0200

    Diversified binning possibilitied fro dimer

--- scripts/stochasticClosure/generateBinningDimer.py
+++ scripts/stochasticClosure/generateBinningDimer.py
@@ -5,7 +5,7 @@ import numpy as np
 from itertools import product
 import deepRD.tools.trajectoryTools as trajectoryTools
 import deepRD.tools.analysisTools as analysisTools
-from deepRD.noiseSampler import binnedDataDimer
+from deepRD.noiseSampler import binnedDataDimer, binnedData
 
 '''
 Generates binned data structures on several different conditionings for the stochastic closure model.
@@ -13,6 +13,7 @@ Currently implemented on conditioning ri+1 on all the combinations dqi,dpi,vi,ri
 '''
 
 bsize = 8
+useAlternativeConditionals = False
 
 parentDirectory = os.environ['DATA'] + 'stochasticClosure/dimer/boxsize' + str(bsize)+ '/benchmark/'
 fnamebase = parentDirectory + 'simMoriZwanzig_'
@@ -62,25 +63,28 @@ trajs = []
 print("Loading data ...")
 for i in range(nfiles):
     traj = trajectoryTools.loadTrajectory(fnamebase, i)
-    lentraj = np.shape([traj])[1]
     # Compute and add relativeDistance and relative velocity at end of trajectory
-    additionalCondtionings = np.zeros([lentraj,4])
-    for j in range(int(lentraj/2)):
-        x1 = traj[2*j][1:4]
-        x2 = traj[2*j+1][1:4]
-        v1 = traj[2*j][4:7]
-        v2 = traj[2*j+1][4:7]
-        normDeltaX, axisRelVel, normAxisVelCM, normOrthogonalVelCM = calculateAdditionalConditionings(x1,x2,v1,v2)
-        additionalCondtionings[2*j][0] = normDeltaX
-        additionalCondtionings[2*j][1] = axisRelVel
-        additionalCondtionings[2*j][2] = normAxisVelCM
-        additionalCondtionings[2*j][3] = normOrthogonalVelCM
-        additionalCondtionings[2*j+1][0] = 1 * normDeltaX
-        additionalCondtionings[2*j+1][1] = -1 * axisRelVel
-        additionalCondtionings[2*j+1][2] = 1 * normAxisVelCM
-        additionalCondtionings[2*j+1][3] = 1 * normOrthogonalVelCM
-    newtraj = np.concatenate([traj,additionalCondtionings], axis=1)
-    trajs.append(newtraj)
+    if useAlternativeConditionals:
+        lentraj = np.shape([traj])[1]
+        additionalCondtionings = np.zeros([lentraj,4])
+        for j in range(int(lentraj/2)):
+            x1 = traj[2*j][1:4]
+            x2 = traj[2*j+1][1:4]
+            v1 = traj[2*j][4:7]
+            v2 = traj[2*j+1][4:7]
+            normDeltaX, axisRelVel, normAxisVelCM, normOrthogonalVelCM = calculateAdditionalConditionings(x1,x2,v1,v2)
+            additionalCondtionings[2*j][0] = normDeltaX
+            additionalCondtionings[2*j][1] = axisRelVel
+            additionalCondtionings[2*j][2] = normAxisVelCM
+            additionalCondtionings[2*j][3] = normOrthogonalVelCM
+            additionalCondtionings[2*j+1][0] = 1 * normDeltaX
+            additionalCondtionings[2*j+1][1] = -1 * axisRelVel
+            additionalCondtionings[2*j+1][2] = 1 * normAxisVelCM
+            additionalCondtionings[2*j+1][3] = 1 * normOrthogonalVelCM
+        newtraj = np.concatenate([traj,additionalCondtionings], axis=1)
+        trajs.append(newtraj)
+    else:
+        trajs.append(traj)
     sys.stdout.write("File " + str(i+1) + " of " + str(nfiles) + " done." + "\r")
 print("\nAll data loaded.")
 print(' ')
@@ -92,20 +96,35 @@ numbins1 = 30 #50
 numbins2 = 10 #50
 numbins3 = 5 #50
 nsigma1 = 3 # Only include up to nsigma standard deviations around mean of data. If no value given, includes all.
-nsigma2 = 2
+nsigma2 = 3 #2
 nsigma3 = 2
 
 # Add elements to parameter dictionary
 parameterDictionary['lagTimesteps'] = lagTimesteps
 
 # List of possible combinations for binnings
+binPositionList = [False] #[False, True]
+binVelocitiesList = [True]
+numBinnedAuxVarsList = [0,1,2] #[0,1] #[0,1,2]
+
+# List of alternative possible combinations for binnings
 #binPositionList = [False] #[False, True]
 binRelativeDistanceList = [True]
 binRelSpeedList = [False, True]
 binVelCenterMassList = [False, True]
 numBinnedAuxVarsList = [0,1,2] #[0,1] #[0,1,2]
 
-def getNumberConditionedVariables(binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars):
+def getNumberConditionedVariables(binPosition, binVelocity, numBinnedAuxVars):
+    numConditionedVariables = 0
+    if binPosition:
+        numConditionedVariables += 1
+    if binVelocity:
+        numConditionedVariables += 1
+    for i in range(numBinnedAuxVars):
+        numConditionedVariables += 1
+    return numConditionedVariables
+
+def getNumberConditionedVariablesAlternative(binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars):
     numConditionedVariables = 0
     if binRelDist:
         numConditionedVariables += 1 # One dimensional, so we assume it doesn't count
@@ -117,45 +136,74 @@ def getNumberConditionedVariables(binRelDist, binRelSpeed, binVelCenterMass, num
         numConditionedVariables += 3
     return numConditionedVariables
 
-for parameterCombination in product(*[binRelativeDistanceList, binRelSpeedList, binVelCenterMassList, numBinnedAuxVarsList]):
-    if parameterCombination != (False,False,False,0):
-        binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars = parameterCombination
-        numConditionedVariables = getNumberConditionedVariables(binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars)
-        if numConditionedVariables <= 4:
-            dataOnBins = binnedDataDimer(boxsizeBinning, numbins1, lagTimesteps, binRelDistance = binRelDist,
-                                    binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
-                                         numBinnedAuxVars=numBinnedAuxVars)
-            dataOnBins.loadData(trajs, nsigma1)
-            print(dataOnBins.boxsize)
-            print(dataOnBins.bins)
-            parameterDictionary['numbins'] = numbins1
-            parameterDictionary['nsigma'] = nsigma1
-        elif numConditionedVariables <= 7:
-            dataOnBins = binnedDataDimer(boxsizeBinning, numbins2, lagTimesteps, binRelDistance = binRelDist,
-                                         binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
-                                         numBinnedAuxVars=numBinnedAuxVars)
-            dataOnBins.loadData(trajs, nsigma2)
-            print(dataOnBins.boxsize)
-            print(dataOnBins.bins)
-            parameterDictionary['numbins'] = numbins2
-            parameterDictionary['nsigma'] = nsigma2
-        else:
-            dataOnBins = binnedDataDimer(boxsizeBinning, numbins3, lagTimesteps, binRelDistance = binRelDist,
-                                    binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
-                                         numBinnedAuxVars=numBinnedAuxVars)
-            dataOnBins.loadData(trajs, nsigma3)
-            print(dataOnBins.boxsize)
-            print(dataOnBins.bins)
-            parameterDictionary['numbins'] = numbins3
-            parameterDictionary['nsigma'] = nsigma3
-        parameterDictionary['percentageOccupiedBins'] = dataOnBins.percentageOccupiedBins
-        dataOnBins.parameterDictionary = parameterDictionary
+if useAlternativeConditionals:
+    for parameterCombination in product(*[binRelativeDistanceList, binRelSpeedList, binVelCenterMassList, numBinnedAuxVarsList]):
+        if parameterCombination != (False,False,False,0):
+            binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars = parameterCombination
+            numConditionedVariables = getNumberConditionedVariablesAlternative(binRelDist, binRelSpeed, binVelCenterMass, numBinnedAuxVars)
+            if numConditionedVariables <= 4:
+                dataOnBins = binnedDataDimer(boxsizeBinning, numbins1, lagTimesteps, binRelDistance = binRelDist,
+                                        binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
+                                             numBinnedAuxVars=numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma1)
+                parameterDictionary['numbins'] = numbins1
+                parameterDictionary['nsigma'] = nsigma1
+            elif numConditionedVariables <= 7:
+                dataOnBins = binnedDataDimer(boxsizeBinning, numbins2, lagTimesteps, binRelDistance = binRelDist,
+                                             binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
+                                             numBinnedAuxVars=numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma2)
+                parameterDictionary['numbins'] = numbins2
+                parameterDictionary['nsigma'] = nsigma2
+            else:
+                dataOnBins = binnedDataDimer(boxsizeBinning, numbins3, lagTimesteps, binRelDistance = binRelDist,
+                                        binRelSpeed = binRelSpeed, binVelCenterMass = binVelCenterMass,
+                                             numBinnedAuxVars=numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma3)
+                parameterDictionary['numbins'] = numbins3
+                parameterDictionary['nsigma'] = nsigma3
+            parameterDictionary['percentageOccupiedBins'] = dataOnBins.percentageOccupiedBins
+            dataOnBins.parameterDictionary = parameterDictionary
 
-        # Dump qi binned data into pickle file and free memory
-        print('Dumping data into pickle file ...')
-        conditionedVarsString = dataOnBins.binningLabel2
-        binnedDataFilename = binningDataDirectory + conditionedVarsString + 'BinnedData.pickle'
-        pickle.dump(dataOnBins, open(binnedDataFilename, "wb"))
-        print('Binning for ' + dataOnBins.binningLabel + ' done.')
-        print(' ')
-        del dataOnBins
+            # Dump qi binned data into pickle file and free memory
+            print('Dumping data into pickle file ...')
+            conditionedVarsString = dataOnBins.binningLabel2
+            binnedDataFilename = binningDataDirectory + conditionedVarsString + 'BinnedData.pickle'
+            pickle.dump(dataOnBins, open(binnedDataFilename, "wb"))
+            print('Binning for ' + dataOnBins.binningLabel + ' done.')
+            print(' ')
+            del dataOnBins
+else:
+    for parameterCombination in product(*[binPositionList, binVelocitiesList, numBinnedAuxVarsList]):
+        if parameterCombination != (False, False, 0):
+            binPosition, binVelocity, numBinnedAuxVars = parameterCombination
+            numConditionedVariables = getNumberConditionedVariables(binPosition, binVelocity, numBinnedAuxVars)
+            if numConditionedVariables == 1:
+                dataOnBins = binnedData(boxsizeBinning, numbins1, lagTimesteps, binPosition, binVelocity,
+                                        numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma1)
+                parameterDictionary['numbins'] = numbins1
+                parameterDictionary['nsigma'] = nsigma1
+            elif numConditionedVariables == 2:
+                dataOnBins = binnedData(boxsizeBinning, numbins2, lagTimesteps, binPosition, binVelocity,
+                                        numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma2)
+                parameterDictionary['numbins'] = numbins2
+                parameterDictionary['nsigma'] = nsigma2
+            else:
+                dataOnBins = binnedData(boxsizeBinning, numbins3, lagTimesteps, binPosition, binVelocity,
+                                        numBinnedAuxVars)
+                dataOnBins.loadData(trajs, nsigma3)
+                parameterDictionary['numbins'] = numbins3
+                parameterDictionary['nsigma'] = nsigma3
+            parameterDictionary['percentageOccupiedBins'] = dataOnBins.percentageOccupiedBins
+            dataOnBins.parameterDictionary = parameterDictionary
+
+            # Dump qi binned data into pickle file and free memory
+            print('Dumping data into pickle file ...')
+            conditionedVarsString = dataOnBins.binningLabel2
+            binnedDataFilename = binningDataDirectory + conditionedVarsString + 'BinnedData.pickle'
+            pickle.dump(dataOnBins, open(binnedDataFilename, "wb"))
+            print('Binning for ' + dataOnBins.binningLabel + ' done.')
+            print(' ')
+            del dataOnBins
